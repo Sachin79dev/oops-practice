14291169aa4b78e84703acfb28640702661f8ced
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-27 22:19:24 +0530

    Add examples of 'this' binding in JavaScript
    Add examples of 'this' binding in JavaScript

---
+++ app.js
@@ -0,0 +1,98 @@
+// Hello OOPs
+
+
+// Understanding this in an Object
+
+
+const user = {
+    name: "Ritik",
+    greet() {
+        console.log(this.name);
+    }
+};
+
+
+user.greet(); // "Ritik"
+
+
+
+
+
+// Default Binding
+
+
+"use strict";
+
+function show() {
+    console.log(this);
+}
+
+show();
+
+
+// First: The browser will print the window API,  Second: The node will print the node API, Third: The "use strict" will print undefined.
+
+
+
+
+
+
+
+// call() in OOPs 
+
+
+function introduce() {
+    console.log(this.name);
+}
+
+
+const person = {
+    name: "Sachin"
+}
+
+
+introduce.call(person) // Sachin
+
+
+
+
+
+
+
+// apply() in OOPs
+
+
+function introduce(city, country) {
+    console.log(`${this.name} from ${city} (${country})`);
+}
+
+
+const person = {
+    name: "Sachin"
+}
+
+// introduce.call(person, "Agra", "India") // Sachin from Agra (India)
+introduce.apply(person, ["Agra", "India"]) // Sachin from Agra (India)
+
+
+
+
+
+
+
+// Fix Lost this
+
+
+
+const user = {
+    name: "Sachin",
+    greet() {
+        console.log(this.name);
+    }
+}
+
+
+
+const fn = user.greet.bind(user);
+
+fn(); // Sachin
